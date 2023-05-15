21ce0889b133d8b76e55b504b150bf5f6fa2374a
VitalijDonets <donezvitalij@gmail.com>
2023-05-15 12:39:57 +0300

    return old window size

--- Interface/src/UI/MyFrame.java
+++ Interface/src/UI/MyFrame.java
@@ -45,7 +45,7 @@ public class MyFrame extends JFrame implements ActionListener {
         textFieldDiapasonRight.setText("5");
 
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setSize(650, 600);
+        this.setSize(800, 600);
         this.setVisible(true);
         this.setResizable(false);
 
