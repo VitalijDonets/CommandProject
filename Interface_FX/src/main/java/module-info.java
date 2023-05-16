bac4e7c30acfabcfb7ccd9f045878af88986da24
VitalijDonets <89968220+VitalijDonets@users.noreply.github.com>
2023-05-16 09:29:31 +0300

    Delete Interface_FX directory

--- Interface_FX/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.interface_fx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires org.junit.jupiter.api;
-
-
-    opens com.example.interface_fx to javafx.fxml;
-    exports com.example.interface_fx;
-}
